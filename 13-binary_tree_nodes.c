9b19faa280036f354073ee46f6650b9120fbac3a
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2023-11-01 11:16:31 +0200

    nodes with at least 1 child in a binary tree

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts the nodes that
+ * have atleast 1 child
+ * @tree: treee
+ * Return: c or NULL
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t c;
+
+	if (!tree)
+		return (0);
+	c = 0;
+	if (!tree->left || !tree->right)
+	{
+		c += binary_tree_nodes(tree->left);
+		c += binary_tree_nodes(tree->right);
+	}
+	else
+		c = 1;
+	return (c);
+}
