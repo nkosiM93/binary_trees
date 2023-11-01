796aa2023a3dabfc23080f6fb27ee94ae05bb09e
Oradell35 <122868692+Odarell35@users.noreply.github.com>
2023-11-01 13:14:47 +0200

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,7 +13,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	c = 0;
-	if (!tree->left || !tree->right)
+	if (tree->left || tree->right)
 	{
 		c += binary_tree_nodes(tree->left);
 		c += binary_tree_nodes(tree->right);
