6d04ea61ecbe8975f5121f8fd83b109b1848afcb
AlphaCoder <mathiyathokozanin@gmail.com>
2023-10-31 20:17:41 -0700

    6-9 and Header File

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_preorder - Implements the pre-oder traversal method
  * @tree: The pointer to the tree being traversed
- * @func: Points to a function that prints the value of each node as we traverse
+ * @func: Points to a function that prints the value of each node as we travers
  */
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
